69ff262710837c120d8a207f38efbc77e0775d5f
Saleh-chanselme <saleh.chanselme@gmail.com>
2025-08-23 14:43:24 +0200

    brief is done !

--- stroke_api/filters.py
+++ stroke_api/filters.py
@@ -1,14 +1,40 @@
 from typing import Optional
 import pandas as pd
-
+import numpy as np
 # Chargement des données (une fois)
+stroke_data_df = pd.read_parquet("stroke_api/data/clean_health.parquet")
 
-# Tester l'app avec :
-# poetry run fastapi dev stroke_api/main.py
-# http://127.0.0.1:8000/docs : utiliser la fonctionnalité Try it out pour tester les routes
 
-# Ajout des fonctions de filtrage des données cf notebook 1
-# Ensuite faire appel à ces fonctions dans le fichier api.py où sont définies les routes.
+# function that gets patients info by (stroke, gender and age) filters
+def filter_patient(
+    gender: Optional[str] = None,
+    stroke: Optional[int] = None,
+    max_age: Optional[int] = None
+):
 
-# Ajouter les fonctions de filtrage pour les autres routes.
+    df = stroke_data_df.copy()
+    
+    if stroke is not None :
+        df = df.loc[df['stroke'] == stroke ]
+    if gender is not None :
+        df = df.loc[df['gender'] == gender]
+    if max_age is not None : 
+        df = df.loc[df['age'] <= max_age]
+        
+
+    return df.to_dict(orient='records')
+
+
+
+# function to get patient info by his ID 
+def get_info_by_id(patient_id: int):
+    
+    df = stroke_data_df.copy()
+    
+    if patient_id is not None:
+        
+        df = df.loc[df['id'] == patient_id]
+        
+    return df.to_dict(orient='records')
+
 
